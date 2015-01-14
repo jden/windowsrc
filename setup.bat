a2a8f4f9773bcf2cd566f66e36c8861f5ae7498c
jden <jason@denizac.org>
2015-01-14 12:34:31 -0800

    add sublimetext3.packagecontrol

--- setup.bat
+++ setup.bat
@@ -1,14 +1,15 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
-chocolatey install googlechrome
-chocolatey install git
-chocolatey install sublimetext3
-chocolatey install virtualbox
-chocolatey install firefox
-chocolatey install deluge
-chocolatey install nodejs
-chocolatey install iojs
-chocolatey install curl
-chocolatey install windirstat
-chocolatey install conemu
-chocolatey install vagrant
+choco install googlechrome
+choco install git
+choco install sublimetext3
+choco install sublimetext3.packagecontrol
+choco install virtualbox
+choco install firefox
+choco install deluge
+choco install nodejs
+choco install iojs
+choco install curl
+choco install windirstat
+choco install conemu
+choco install vagrant
